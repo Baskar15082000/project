ba468dbcb5b6f2b7c2f13f18c8ab82de4692ec23
Baskar15082000 <134188943+Baskar15082000@users.noreply.github.com>
2023-09-01 19:26:20 +0530

    Delete Snake_and_Ladder directory

--- Snake_and_Ladder/src/main/java/com/example/snake_and_ladder/Dices.java
+++
@@ -1,7 +0,0 @@
-package com.example.snake_and_ladder;
-
-public class Dices {
-    public int getrooldie(){
-        return  (int)(Math.random()*6+1);
-    }
-}
--- Snake_and_Ladder/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.snake_and_ladder {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.snake_and_ladder to javafx.fxml;
-    exports com.example.snake_and_ladder;
-}
